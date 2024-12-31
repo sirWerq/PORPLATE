212cdc71b1adf76f1d8d959fc9608f2339d96e89
sirWerq <rifqx1904@gmail.com>
2024-12-31 15:06:24 +0700

    bug fixed

--- src/pages/ProjectsPage.jsx
+++
@@ -1,9 +0,0 @@
-import ProjectListComponent from '../components/listProject';
-
-export default function ProjectPage() {
-    return (
-        <div className='w-full h-full px-2 py-28 lg:px-7 flex flex-col gap-10 items-center dark:bg-[#121212] dark:text-white'>
-            <ProjectListComponent />
-        </div>
-    );
-}
--- src/store/store.js
+++
@@ -1,8 +0,0 @@
-import { create } from 'zustand';
-
-const useStore = create((set) => ({
-    darkMode: false,
-    setDarkMode: () => set((state) => ({ darkMode: !state.darkMode })),
-}));
-
-export default useStore;
